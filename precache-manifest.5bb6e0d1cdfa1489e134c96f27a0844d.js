26ececc42e646195fea6a80237c84ced6a2bc504
Max Zanelato <maxzanelato@gmail.com>
2020-04-14 12:36:41 -0300

    Updates

---
+++ precache-manifest.5bb6e0d1cdfa1489e134c96f27a0844d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "173aa90a883cc283c4326449cebf450a",
+    "url": "/poc-antd-reactjs/index.html"
+  },
+  {
+    "revision": "acd9fe5b7d5affea225b",
+    "url": "/poc-antd-reactjs/static/css/2.ab081279.chunk.css"
+  },
+  {
+    "revision": "3f0572402c14bd72f5fb",
+    "url": "/poc-antd-reactjs/static/css/main.b8f5f1be.chunk.css"
+  },
+  {
+    "revision": "acd9fe5b7d5affea225b",
+    "url": "/poc-antd-reactjs/static/js/2.e76a1752.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/poc-antd-reactjs/static/js/2.e76a1752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f0572402c14bd72f5fb",
+    "url": "/poc-antd-reactjs/static/js/main.9ef066fc.chunk.js"
+  },
+  {
+    "revision": "39e1c7f68d3431748e8a",
+    "url": "/poc-antd-reactjs/static/js/runtime-main.13fb57ab.js"
+  },
+  {
+    "revision": "bbc9144f90b5936fe9c6bad6b83c55ed",
+    "url": "/poc-antd-reactjs/static/media/avatar.bbc9144f.png"
+  }
+]);
--- src/interfaces/IMenuItem.ts
+++
@@ -1,9 +0,0 @@
-export interface IMenuItem {
-  id: number;
-  icon: any;
-  text: string;
-}
-
-export interface OwnPropsIMenuItem {
-  menuItem: Array<IMenuItem>;
-}
